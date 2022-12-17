c1cc9671c864632e41ba21552d9015203bdb1935
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-12-17 19:04:17 +0100

    init project

---
+++ microservice-DataObject/src/main/java/org/amt/microservicedataobject/MicroserviceDataObjectApplication.java
@@ -0,0 +1,13 @@
+package org.amt.microservicedataobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDataObjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDataObjectApplication.class, args);
+    }
+
+}
---
+++ microservice-DataObject/src/test/java/org/amt/microservicedataobject/MicroserviceDataObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amt.microservicedataobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDataObjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
